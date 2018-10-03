fb867d4460918dd984dec5ac6912ed4d47b46551
daniro-san <danilao.rs@gmail.com>
2018-10-03 14:13:16 -0300

    added style and asset files

---
+++ assets/fonte.js
@@ -0,0 +1,7 @@
+
+ WebFont.load({
+            google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+          }); 
