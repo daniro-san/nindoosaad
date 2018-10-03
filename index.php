d86b4b2fa4e32d246cbf4a8090885fe1e55ca4d1
daniro-san <danilao.rs@gmail.com>
2018-10-03 14:29:19 -0300

    changed scripts location

--- index.php
+++ index.php
@@ -6,9 +6,19 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>SAAD</title>
 
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-  <link rel="stylesheet" href="bootstrap.min.css">
-  <link rel="stylesheet" href="Site.css">
+  <link href="assets/vendors/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet"/>
+
+
+  <link href="assets/vendors/base/vendors.bundle.css" rel="stylesheet"/>
+
+
+  <link href="assets/demo/demo2/base/style.bundle.css" rel="stylesheet"/>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+
+  <script src="assets/fonte.js"></script>
+
 
   <style>
     html,
@@ -41,7 +51,7 @@
         <div class="text-center pt-2 mt-2 text-message font-weight-regular">
           <div class="col-12 table-responsive"><h3 id="companyData"></h3></div>
           <table id="resultTable" class="table">
-            <thead class="thead-light">
+            <thead>
               <tr>
                 <th>Data da consulta</th>
                 <th>Tipo de documento</th>
@@ -59,9 +69,16 @@
       </main>
   </div>
 
-  <script src="//code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  <script src="assets/vendors/base/vendors.bundle.js"></script>
+
+  <script src="assets/demo/demo2/base/scripts.bundle.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+
+  <script src="assets/app/js/dashboard.js"></script>
+
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
   <script>
