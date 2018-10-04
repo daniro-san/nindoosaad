1410ec523818fda1ff0e35d0d495f7946b5c546b
daniro-san <danilao.rs@gmail.com>
2018-10-04 17:28:46 -0300

    changed main element class

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
   </header>
   
   <div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver-desktop m-grid--desktop 	m-container m-container--responsive m-container--xxl m-page__container m-body">
-      <main role="main" class="inner cover container-fluid h-100">
+      <main role="main" class="m-grid__item m-grid__item--fluid m-wrapper">
         <div class="row align-items-center justify-content-center h-50 d-none">
           <div class="col col-sm-8 col-md-8 col-lg-6 col-xl-4">
             <form action="">
@@ -124,7 +124,7 @@
         <div class="text-center pt-2 mt-2 text-message font-weight-regular">
           <div class="col-12 table-responsive"><h3 id="companyData"></h3></div>
           <table id="resultTable" class="table">
-            <thead class="thead-light">
+            <thead class="">
               <tr>
                 <th>Data da consulta</th>
                 <th>Tipo de documento</th>
